f3a6d35b631ac7e76be18034f0dcb5ab08a38a86
Ivan Kritsckii <ivan.kritsckii@yandex.ru>
2024-07-29 06:52:16 +0500

    feat: start next.js

--- start_react/vite.config.ts
+++ start_react/vite.config.ts
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig, loadEnv } from 'vite'
 
-export default defineConfig(() => {
+
+
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
     return {
+      define: {
+        'process.env.SOME_KEY': JSON.stringify(env.SOME_KEY)
+      },
         plugins: [react(), tsconfigPaths()],
-        base: '/react',
+        base: '/react/',
         test: {
           globals: true,
           environment: 'jsdom',
